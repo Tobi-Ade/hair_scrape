3a7a40a667b1c147888ffe1ca8db9ebcfd7cf164
tobi-ade <themarveloustobi@gmail.com>
2024-03-06 18:02:06 +0100

    finished test

--- test.py
+++ test.py
@@ -34,84 +34,114 @@ def scrape():
     print(browser.current_url)
     data_list = {}
     try:
-        home_page = browser.current_window_handle
-        products_links = browser.find_elements(By.XPATH, '//div[@class="nr-details"]')
-        page_products = [product.find_element(By.TAG_NAME, 'a') for product in products_links]
-        next_page = browser.find_element(By.XPATH, '//li//a[@class="next page-numbers"]')
-        time.sleep(10)
         # wait = WebDriverWait(browser, 10)
         # wait.until(EC.presence_of_all_elements_located((By.XPATH, '//li//a[@class="next page-numbers"]')))
         product_brand = "tgin"
-        
-        for product in page_products:
-            time.sleep(5)
-            wait = WebDriverWait(browser, 10)
-            wait.until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="nr-details"]')))
-            final_data = []
-            time.sleep(5)
-            product.send_keys(Keys.CONTROL + Keys.RETURN)
-            time.sleep(5)
-            browser.switch_to.window(browser.window_handles[-1])
-            print(browser.current_url)
-            product_page = browser.current_window_handle
-            product_name = browser.find_element(By.XPATH, '//div[@class="pro-detail-tgin-text"]//h1').text
-            print(f"getting data for {product_name}...")
-            if "Green Tea Super Moist Leave in Conditioner" in product_name:
-                info = browser.find_element(By.XPATH, '//div[@class="product-info-content-column"]//div[@class="inner"]')
-                product_desc = browser.find_element(By.XPATH, '//div[@class="pro-detail-tgin-text"]//p').text
-                product_directions = info.find_elements(By.TAG_NAME, 'p')[4].text.split("Directions:")[1]
-                product_ingredients = info.find_elements(By.TAG_NAME, 'p')[5].text.split("Ingredients:")[1]
-            else:
-                info = browser.find_element(By.XPATH, '//div[@class="product-info-content-column"]//div[@class="inner"]')
-                product_desc = browser.find_element(By.XPATH, '//div[@class="pro-detail-tgin-text"]//p').text
-                product_directions = info.find_elements(By.TAG_NAME, 'p')[2].text.split("Directions:")[1]
-                product_ingredients = info.find_elements(By.TAG_NAME, 'p')[3].text.split("Ingredients:")[1]
-            reviews_button = browser.find_element(By.XPATH, '//button[@id="tab-3"]')
-            reviews_button.click()
-            time.sleep(5)
-            reviews = browser.find_elements(By.XPATH, './/div[@class="stamped-reviews"]//div[@class="stamped-review"]')
-            for review in reviews:
-                header = review.find_element(By.XPATH, './/div[@class="stamped-review-header"]')
-                content = review.find_element(By.XPATH, './/div[@class="stamped-review-content"]')
-                review_topic = content.find_element(By.XPATH, './/h3').text
-                reviewer_name = header.find_element(By.XPATH, './/strong[@class="author"]').text
-                review_content = content.find_element(By.XPATH, './/p[@class="stamped-review-content-body"]').text
-                review_date = header.find_element(By.XPATH, './/div[@class="created"]').text
-                review_rating = str(content.find_element(By.XPATH, './/span').get_attribute("outerHTML").split(" ")[4]).split("=")[-1].split('"')[1].split('"')[0]
-                # break
-                data = {
-                        'product_brand': product_brand,
-                        'product_name': product_name,
-                        'product_ingredients': product_ingredients,
-                        'product_desc': product_desc,
-                        'product_directions': product_directions,
-                        'reviewer_name': reviewer_name,
-                        'review_topic': review_topic,
-                        'review_comment': review_content,
-                        'review_date': review_date,
-                        'review_rating': review_rating 
-
-                        } 
-                final_data.append(data)
-            browser.close()
+        while product_brand:
             try:
-                print(final_data)
+                current_page = browser.current_window_handle
                 print()
-                print()
-            except UnicodeEncodeError:
-                print(str(final_data).encode('cp1252', errors='ignore'))
-            # break
-            data_list[product_name] = final_data
+                products_links = browser.find_elements(By.XPATH, '//div[@class="nr-details"]')
+                page_products = [product.find_element(By.TAG_NAME, 'a') for product in products_links]
+                time.sleep(10)
+                next_page = browser.find_element(By.XPATH, '//li//a[@class="next page-numbers"]')
+                for product in page_products:
+                    time.sleep(5)
+                    wait = WebDriverWait(browser, 10)
+                    wait.until(EC.presence_of_all_elements_located((By.XPATH, '//div[@class="nr-details"]')))
+                    final_data = []
+                    time.sleep(5)
+                    product.send_keys(Keys.CONTROL + Keys.RETURN)
+                    time.sleep(5)
+                    browser.switch_to.window(browser.window_handles[-1])
+                    print(browser.current_url)
+                    product_page = browser.current_window_handle
+                    product_name = browser.find_element(By.XPATH, '//div[@class="pro-detail-tgin-text"]//h1').text
+                    print(f"getting data for {product_name}...")
+                    if "Green Tea Super Moist Leave in Conditioner" in product_name:
+                        info = browser.find_element(By.XPATH, '//div[@class="product-info-content-column"]//div[@class="inner"]')
+                        product_desc = browser.find_element(By.XPATH, '//div[@class="pro-detail-tgin-text"]//p').text
+                        product_directions = info.find_elements(By.TAG_NAME, 'p')[4].text.split("Directions:")[1]
+                        product_ingredients = info.find_elements(By.TAG_NAME, 'p')[5].text.split("Ingredients:")[1]
+                    else:
+                        info = browser.find_element(By.XPATH, '//div[@class="product-info-content-column"]//div[@class="inner"]')
+                        product_desc = browser.find_element(By.XPATH, '//div[@class="pro-detail-tgin-text"]//p').text
+                        product_directions = info.find_elements(By.TAG_NAME, 'p')[2].text.split("Directions:")[1]
+                        product_ingredients = info.find_elements(By.TAG_NAME, 'p')[3].text.split("Ingredients:")[1]
+                    reviews_button = browser.find_element(By.XPATH, '//button[@id="tab-3"]')
+                    reviews_button.click()
+                    time.sleep(5)
+                    reviews = browser.find_elements(By.XPATH, './/div[@class="stamped-reviews"]//div[@class="stamped-review"]')
+                    while product_page:
+                        try:
+                            reviews = browser.find_elements(By.XPATH, './/div[@class="stamped-reviews"]//div[@class="stamped-review"]')
+                            for review in reviews:
+                                header = review.find_element(By.XPATH, './/div[@class="stamped-review-header"]')
+                                content = review.find_element(By.XPATH, './/div[@class="stamped-review-content"]')
+                                review_topic = content.find_element(By.XPATH, './/h3').text
+                                reviewer_name = header.find_element(By.XPATH, './/strong[@class="author"]').text
+                                review_content = content.find_element(By.XPATH, './/p[@class="stamped-review-content-body"]').text
+                                review_date = header.find_element(By.XPATH, './/div[@class="created"]').text
+                                review_rating = str(content.find_element(By.XPATH, './/span').get_attribute("outerHTML").split(" ")[4]).split("=")[-1].split('"')[1].split('"')[0]
+                                # break
+                                data = {
+                                        'product_brand': product_brand,
+                                        'product_name': product_name,
+                                        'product_ingredients': product_ingredients,
+                                        'product_desc': product_desc,
+                                        'product_directions': product_directions,
+                                        'reviewer_name': reviewer_name,
+                                        'review_topic': review_topic,
+                                        'review_comment': review_content,
+                                        'review_date': review_date,
+                                        'review_rating': review_rating 
+
+                                        } 
+                                if data not in final_data:
+                                    final_data.append(data)
+                            wait = WebDriverWait(browser, 10, ignored_exceptions=(NoSuchElementException, StaleElementReferenceException))      
+                            wait.until(EC.presence_of_all_elements_located((By.XPATH, '//li[@class="next"]/a')))
+                            next_rev_page = browser.find_element(By.XPATH, '//li[@class="next"]/a')
+                            print(next_rev_page.get_attribute('outerHTML'))
+                            next_rev_page.send_keys(Keys.CONTROL + Keys.RETURN)
+                            time.sleep(5)
+                        except (NoSuchElementException, TimeoutException):
+                            print("Last Review Page")
+                            print()
+                            break
+                    
+                    browser.close()
+                    try:
+                        print(final_data)
+                        print()
+                        print()
+                    except (NoSuchElementException, StaleElementReferenceException):
+                        print("Last Review Page")
+                        break 
+                    except UnicodeEncodeError:
+                        print(str(final_data).encode('cp1252', errors='ignore'))
+                    # break
+                    data_list[product_name] = final_data
+                    
+                        # break
+                    browser.switch_to.window(current_page)
+                time.sleep(5)
+                wait = WebDriverWait(browser, 10, ignored_exceptions=(NoSuchElementException, StaleElementReferenceException))      
+                wait.until(EC.presence_of_all_elements_located((By.XPATH, '//li//a[@class="next page-numbers"]')))
+                next_page = browser.find_element(By.XPATH, '//li//a[@class="next page-numbers"]')
+                next_page.send_keys(Keys.CONTROL + Keys.RETURN)
+                time.sleep(5)
+                browser.switch_to.window(browser.window_handles[-1])
+                print(browser.current_url)
+                time.sleep(5)
+                break
+            except(NoSuchElementException, StaleElementReferenceException):
+                    print("Last Product Page")
+                    break
             
-                # break
-            browser.switch_to.window(home_page)
-            break
-            
-       
     except Exception as e:
         print(e)
         traceback.print_exc()
     
     browser.quit()
-
 scrape()
